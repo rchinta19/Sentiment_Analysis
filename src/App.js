4fcdae1ccb40e87e8030618e4f8e1f20ec5253cc
rchinta19 <rchinta@appstekcorp.com>
2022-08-11 18:34:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import * as d3 from "d3";
+import BarChart from "./Components/BarChart";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>D3 Implementation</h1>
       </header>
+      <div>
+        <BarChart options={ {}} data={[{day:"Monday",totalBirds:20},{day:"Tuesday",totalBirds:34},,{day:"Wednsday",totalBirds:52}] />
+      </div>
     </div>
   );
 }
